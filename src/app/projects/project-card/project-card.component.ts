db669b962ca08ee0730549113d059e523d4e9ce0
svillagracia <svillagracia@gmail.com>
2018-01-14 04:43:59 -0800

    feature: projects

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+declare interface Project {
+  image: string;
+  name: string;
+  year: number;
+  employer: string;
+  role: string[];
+}
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.less']
 })
+
 export class ProjectCardComponent implements OnInit {
+  @Input() project: Project;
 
   constructor() { }
 
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface Project {
+  image: string;
+  name: string;
+  year: number;
+  employer: string;
+  role: string[];
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.less']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  public projects: Project[];
+  constructor() {
+    this.projects = [
+      {
+        image: '',
+        name: 'Plymouth Leadership College',
+        year: 2017,
+        employer: 'Republic Development & Design',
+        role: [ 'developer', 'project manager']
+      },
+      {
+        image: '',
+        name: 'Nomadic Admin Portal',
+        year: 2017,
+        employer: 'Nomadic Learning (contract)',
+        role: [ 'lead front end developer' ]
+      },
+      {
+        image: '',
+        name: 'Alaska InFlight Application',
+        year: 2015,
+        employer: 'Indigo Slate',
+        role: ['front end developer']
+      },
+      {
+        image: '',
+        name: 'Alaska Airlines Styleguide',
+        year: 2016,
+        employer: 'Indigo Slate',
+        role: [ 'developer' ]
+      },
+      {
+        image: '',
+        name: 'Microsoft Cyber Secuirty Quiz',
+        year: 2015,
+        employer: 'Indigo Slate',
+        role: [ 'front end developer' ]
+      }
+    ];
+  }
 
   ngOnInit() {
   }
